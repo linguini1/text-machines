83bb0ed5121fe0f23db8d1a0da03ce2283de18f2
Matteo Golin <matteo.golin@gmail.com>
2025-12-15 16:49:54 -0500

    In-text docs for user-facing functions.

--- common/api.h
+++ common/api.h
@@ -9,8 +9,12 @@ struct txtmac
 {
     /* This function returns the next character to be output by the text
      * machine.
+     *
+     * - tm: A reference to this text machine (itself). Cannot be NULL.
+     *
+     * Returns: The next character to be output by this text machine.
      */
-    char (*next)(struct txtmac *);
+    char (*next)(struct txtmac *tm);
 
     /* This member contains private lower-half information from the text machine
      * implementation. It must be allocated using `malloc` and safe to call
@@ -19,12 +23,44 @@ struct txtmac
     void *priv;
 };
 
+/* Destroys a text machine by releasing its memory.
+ *
+ * - tm: The text machine to destroy
+ */
 void mdestroy(struct txtmac *tm);
 
 /* Sink options for machine output */
 
+/* Sinks the output of a text machine to a file pointed to by a file descriptor.
+ *
+ * - tm: The text machine to collect the output of
+ * - fd: The file descriptor of the file where the output of `tm` should be
+ *   stored.
+ *
+ * Returns: 0 on success, error code on failure.
+ */
 int sink_fd(struct txtmac *tm, int fd);
+
+/* Sinks the output of a text machine to a file pointed to by a file pointer.
+ *
+ * - tm: The text machine to collect the output of
+ * - fd: The file pointer of the file where the output of `tm` should be
+ *   stored.
+ *
+ * Returns: 0 on success, error code on failure.
+ */
 int sink_file(struct txtmac *tm, FILE *file);
+
+/* Sinks the output of a text machine to a buffer. `EOF` is replaced by the null
+ * terminator '\0'. The buffer size will be respected; this function will only
+ * fill the buffer with up to `n` characters from the text machine.
+ *
+ * - tm: The text machine to collect the output of. Cannot be NULL.
+ * - buf: The buffer where the output of `tm` should be stored. Cannot be NULL.
+ * - n: The size of the buffer in characters.
+ *
+ * Returns: The number of bytes written to `buf`. 0 bytes on error.
+ */
 size_t sink_buf(struct txtmac *tm, char *buf, size_t n);
 
 #endif // _COMMON_API_H_
--- machines/machines.h
+++ machines/machines.h
@@ -8,17 +8,88 @@
 
 /* Generative machines */
 
+/* Creates and initializes a "file" text machine whose output will be drawn from
+ * `src`.
+ *
+ * - src: A non-null FILE pointer where the machine output will be obtained
+ *   from.
+ *
+ * Returns: A pointer to a "file" text machine allocated on the heap, or NULL if
+ * there was an error.
+ */
 struct txtmac *minit_file(FILE *src);
+
+/* Creates and initializes a "file descriptor" text machine whose output will be
+ * drawn from the file associated with `fd`.
+ *
+ * - fd: A non-negative file descriptor where the machine output will be
+ *   obtained from.
+ *
+ * Returns: A pointer to a "file descriptor" text machine allocated on the heap,
+ * or NULL if there was an error.
+ */
 struct txtmac *minit_fd(int fd);
+
+/* Creates and initializes a "buffer" text machine whose output will be
+ * drawn from the provided buffer.
+ *
+ * - buf: A non-null character buffer
+ * - n: The number of characters in `buf`
+ *
+ * Returns: A pointer to a "buffer" text machine allocated on the heap,
+ * or NULL if there was an error.
+ */
 struct txtmac *minit_buf(const char *buf, size_t n);
+
+/* Creates and initializes a "letter" text machine whose output will be a
+ * sequence of a single letter.
+ *
+ * - letter: The letter to output
+ * - count: The number of times to output `letter` before `EOF`. If 0, `letter`
+ *   will be output infinitely many times.
+ *
+ * Returns: A pointer to a "letter" text machine allocated on the heap,
+ * or NULL if there was an error.
+ */
 struct txtmac *minit_letter(char letter, size_t count);
 
 /* Filtering machines */
 
 typedef char(charop)(char);
 
+/* Creates and initializes an "applicator" text machine.
+ *
+ * - tm: The text machine whose output is to be modified by the applicator.
+ *   Cannot be NULL.
+ * - op: The operation applied to each character output by `tm`. Cannot be NULL.
+ *
+ * Returns: A pointer to an "applicator" text machine allocated on the heap,
+ * or NULL if there was an error.
+ */
 struct txtmac *minit_applicator(struct txtmac *tm, charop *op);
+
+/* Creates and initializes a "jumbler" text machine.
+ *
+ * - tm: The text machine whose output is to be jumbled. Cannot be NULL.
+ *
+ * Returns: A pointer to a "jumbler" text machine allocated on the heap,
+ * or NULL if there was an error.
+ */
 struct txtmac *minit_jumbler(struct txtmac *tm);
-struct txtmac *minit_replace(struct txtmac *tm, const char *old, const char *new);
+
+/* Creates and initializes a "replace" text machine.
+ *
+ * - tm: The text machine whose output is to be modified by the "replace" text
+ *   machine. Cannot be NULL.
+ * - old: The term to have all instances replaced by `new`. Can be the empty
+ *   string to have no effect. Cannot be NULL.
+ * - new: The new term to replace all instance of `old`. Can be NULL or empty
+ *   string to have the behaviour of just removing `old`.
+ *
+ * Returns: A pointer to a "replace" text machine allocated on the heap, or NULL
+ * if there was an error.
+ */
+struct txtmac *minit_replace(struct txtmac *tm, const char *old,
+                             const char *new);
 
 #endif // _MACHINES_H_
